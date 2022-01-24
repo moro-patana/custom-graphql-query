bb008e9f2cb04a464e38c50fa6b8b5aca7570312
manjaka <manjaka.ran@onja.org>
2022-01-24 10:45:40 +0300

    creates states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { useGetStudentsQuery } from "./generated/graphql";
+import { useState } from "react";
 import './index.css'
-
+import DeleteStudentModal from "./components/Deletestudent";
 export default function App() {
+  const [open, setOpen] = useState(false)
+  const [studentId, setStudentId] = useState<number>(0);
+
 
  
   const { loading, error, data } = useGetStudentsQuery();
@@ -57,13 +61,17 @@ export default function App() {
                   <tr key={person?.id} className={personIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person?.id}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {/* <img className="h-10 w-10 rounded-full" src={person?.imageUrl} alt="" /> */}
+                      <img className="h-10 w-10 rounded-full" src={person?.imageUrl?.toString()} alt="" />
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person?.firstName}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person?.lastName}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person?.role}</td>
                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M7 6V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5zm6.414 8l1.768-1.768-1.414-1.414L12 12.586l-1.768-1.768-1.414 1.414L10.586 14l-1.768 1.768 1.414 1.414L12 15.414l1.768 1.768 1.414-1.414L13.414 14zM9 4v2h6V4H9z" /></svg></button>
+                      <button onClick={() => {
+                        setOpen(true)
+                        console.log(person?.id)
+                        setStudentId(Number(person?.id))
+                        }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M7 6V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5zm6.414 8l1.768-1.768-1.414-1.414L12 12.586l-1.768-1.768-1.414 1.414L10.586 14l-1.768 1.768 1.414 1.414L12 15.414l1.768 1.768 1.414-1.414L13.414 14zM9 4v2h6V4H9z" /></svg></button>
                       <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z" /><path d="M12.9 6.858l4.242 4.243L7.242 21H3v-4.243l9.9-9.9zm1.414-1.414l2.121-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" /></svg></button>
                     </td>
                   </tr>
@@ -73,6 +81,7 @@ export default function App() {
           </div>
         </div>
       </div>
+      <DeleteStudentModal open={open} setOpen={setOpen} studentId={studentId} setStudentId={setStudentId}/>
     </div>
   )
 }
